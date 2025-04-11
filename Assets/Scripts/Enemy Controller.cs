700390a6a772a333ddc1970f06b552e63cbc6a77
david <david@DESKTOP-KICK3RA>
2025-04-11 04:48:50 -0400

    CO routine commited and finished

--- Assets/Scripts/Enemy Controller.cs
+++ Assets/Scripts/Enemy Controller.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    //Coroutine for enemymovement
     Coroutine enemyMovement;
     // Start is called before the first frame update
     void Start()
     {
+     //Starts the coroutine so that the enemy can begin moving.
         StartCoroutine(Enemymovement());
     }
 
@@ -16,18 +19,29 @@ public class EnemyController : MonoBehaviour
     {
        
     }
+    //Create Coroutine
     IEnumerator Enemymovement()
     {
-        float speed = 1f;
+        //Float for the speed of the enemy
+        float speed = .05f;
+       //Boolean value that checks if the enemy has done it's pause. If it hasn't then it can pause and if it has then it cant pause again
+        bool hasPaused = false;
+       //A while loop so that the movement can be constant frame by frame.
         while (true)
         {
+            //VEctor 3 for enemy movement
             Vector3 enemyPosition = transform.position;
             enemyPosition.x -= speed;
             transform.position = enemyPosition;
-            if (enemyPosition.x == -5f)
+           //Checks the position of the enemy and if it has paused. If it hasn't paused yet then the enemy stops for 2 seconds before moving again
+            if (enemyPosition.x <= -7f && hasPaused == false)
             {
-                yield return new WaitForSeconds(6f);
+                yield return new WaitForSeconds(2f);
+                hasPaused = true;
             }
+           //Yield that prevents unity from CRASHING
+            yield return null;
         }
+       
     }
 }
